776f7c0ca5f06f2d081329785e455f78f28a884a
Avantesb <102500952+Avantesb@users.noreply.github.com>
2022-11-21 12:29:53 +0700

    first commit

---
+++ account-service/src/main/java/com/avantesb/rfidbankmicroservice/App.java
@@ -0,0 +1,13 @@
+package com.avantesb.rfidbankmicroservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
