1acf534d654ae41612dda9c6ac234ae122cb7838
melnikov <3>
2022-11-27 00:17:44 +0700

    configServer add

--- account-service/src/main/java/com/avantesb/rfidbankmicroservice/model/entity/TransactionEntity.java
+++ account-service/src/main/java/com/avantesb/rfidbankmicroservice/model/entity/TransactionEntity.java
@@ -1,10 +1,7 @@
 package com.avantesb.rfidbankmicroservice.model.entity;
 
 import com.avantesb.rfidbankmicroservice.model.constant.TransactionType;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import jakarta.persistence.*;
 import java.math.BigDecimal;
@@ -15,6 +12,7 @@ import java.math.BigDecimal;
 @Builder
 @Table(name = "bank_transaction")
 @NoArgsConstructor
+@AllArgsConstructor
 public class TransactionEntity {
 
     @Id
