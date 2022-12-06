f1d43ae947fbae6c84f4841aebb3239c4a19d964
melnikov <3>
2022-12-06 22:06:34 +0700

    add manual ack in account-bank

--- account-service/src/main/java/com/avantesb/rfidbankmicroservice/sevice/message/MessageService.java
+++ account-service/src/main/java/com/avantesb/rfidbankmicroservice/sevice/message/MessageService.java
@@ -8,46 +8,47 @@ import com.avantesb.rfidbankmicroservice.model.request.TransferRequest;
 import com.avantesb.rfidbankmicroservice.model.response.TransferResponse;
 import com.avantesb.rfidbankmicroservice.sevice.AccountService;
 import com.avantesb.rfidbankmicroservice.sevice.TransactionService;
-import lombok.AllArgsConstructor;
-import org.springframework.amqp.core.AcknowledgeMode;
+import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.integration.StaticMessageHeaderAccessor;
-import org.springframework.integration.acks.AckUtils;
-import org.springframework.integration.acks.AcknowledgmentCallback;
-import org.springframework.integration.acks.SimpleAcknowledgment;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
+@Slf4j
 @Component
-@AllArgsConstructor
 public class MessageService {
+
+    @Value("${idempotencyKey.ttl:120}")
+    private Long IDEMPOTENCY_KEY_TTL;
 
     private AccountService accountService;
     private TransactionService transactionService;
     private IdempKeyRepository idempKeyRepository;
+
+    public MessageService(AccountService accountService, TransactionService transactionService, IdempKeyRepository idempKeyRepository) {
+        this.accountService = accountService;
+        this.transactionService = transactionService;
+        this.idempKeyRepository = idempKeyRepository;
+    }
 
     @Bean
     Function<List<Long>, List<AccountBank>> getAccounts(){
         return input -> accountService.getAccountsByClientId(input);
     }
 
-
-//    @Bean
-//    Function<TransferRequest, TransferResponse> commitTransaction(){
-//        return request -> transactionService.fundTransfer(request);
-//    }
-
     @Bean
     Function<Message<TransferRequest>, TransferResponse > commitTransaction(){
         return message -> {
             System.out.println("in Reddis: \n"+ idempKeyRepository.findAll());
-
+            System.out.println("MESSAGE:\n"+message);
+            Channel channel = message.getHeaders().get(AmqpHeaders.CHANNEL, Channel.class);
+            Long deliveryTag = message.getHeaders().get(AmqpHeaders.DELIVERY_TAG, Long.class);
 
             String key = message.getHeaders().get("Idempotency-Key", String.class);
             Optional<IdempotencyKey> idempotencyKeyOptional = idempKeyRepository.findById(key);
@@ -57,6 +58,7 @@ public class MessageService {
 //                        .transferId(message.getPayload().getTransferId())
 //                        .message("Transaction has already been completed")
 //                        .build();
+                ackMessage(channel, deliveryTag);
                 return transactionService.getSavedTransaction(message.getPayload());
             }
 
@@ -68,7 +70,17 @@ public class MessageService {
                     .build();
             idempKeyRepository.save(idempotencyKey);
 
+                ackMessage(channel, deliveryTag);
             return response;
         };
+    }
+
+    private void ackMessage(Channel channel, Long deliveryTag){
+        try {
+            channel.basicAck(deliveryTag, false);
+            System.out.println("============Acknoledge==============");
+        } catch (Exception e) {
+            log.info("Failed to ack original message", e);
+        }
     }
 }
